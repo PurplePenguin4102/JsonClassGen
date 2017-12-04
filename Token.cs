3939ecdea4444453aa056bfc822e02f3bc51194a
Joey <joseph.h.ray@gmail.com>
2017-12-04 22:11:29 +1000

    Adding TOken

---
+++ Token.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonClassGen
+{
+    internal class Token
+    {
+        internal TokenType Type { get; set; }
+        internal string Value { get; set; }
+    }
+}
