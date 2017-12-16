04c90d3aa5022b4aa67d77bc3b86aa742d29df65
Joey <joseph.h.ray@gmail.com>
2017-12-16 19:59:20 +1000

    Adding to my emitter

--- JsonClassGen/Emitter.cs
+++ JsonClassGen/Emitter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace JsonClassGen
@@ -11,9 +12,46 @@ namespace JsonClassGen
 
         public void EmitClass(List<Token> tokens)
         {
-            var property = string.Format(PropertyFormat, "Billy", "Bob");
-            var classFill = string.Format(ClassFormat, RandomClassName(), property);
+            var properties = GetBasicProperties(tokens);
+            var classFill = string.Format(ClassFormat, RandomClassName(), properties.ToString());
             var codeFile = string.Format(CodeFileFormat, classFill);
+        }
+
+        private StringBuilder GetBasicProperties(List<Token> tokens)
+        {
+            var properties = new StringBuilder();
+            var basTokens = tokens.Where(t => t.Type != TokenType.Array && t.Type != TokenType.Object);
+            foreach (var tok in basTokens)
+            {
+                switch (tok.Type)
+                {
+                    case TokenType.Null: properties.Append(BasicObjectProperty(tok)); break;
+                    case TokenType.Number: properties.Append(BasicNumberProperty(tok)); break;
+                    case TokenType.Boolean: properties.Append(BasicBooleanProperty(tok)); break;
+                    case TokenType.String: properties.Append(BasicStringProperty(tok)); break;
+                }
+            }
+            return properties;
+        }
+
+        private string BasicStringProperty(Token tok)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string BasicBooleanProperty(Token tok)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string BasicNumberProperty(Token tok)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string BasicObjectProperty(Token tok)
+        {
+            throw new NotImplementedException();
         }
 
         private string RandomClassName()
@@ -37,8 +75,8 @@ using System.Text;
 
 namespace JsonClassGen.Entities
 {{
-    {0}
+{0}
 }}";
-        private string PropertyFormat = "public {0}? {1} {{ get; set; }}";
+        private string PropertyFormat = "\t\t\tpublic {0} {1} {{ get; set; }}";
     }
 }
